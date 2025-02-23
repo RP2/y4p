558e399684152969d31d76d28d537db1af9981e8
RP2 <RP2@users.noreply.github.com>
2025-02-23 13:10:09 -0800

    basic project setup with prettier, sitemap, a custom font, & tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://y4p.rileyperalta.com",
+  vite: { plugins: [tailwindcss()] },
+  integrations: [sitemap()],
+});
